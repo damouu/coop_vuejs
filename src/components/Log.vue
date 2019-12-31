6ed168795f2d8770f4ac5d19b24c4d560a67fd4a
sehbaoui <mouad>
2019-12-31 17:36:16 +0100

    practice with another API

--- src/components/Log.vue
+++ src/components/Log.vue
@@ -1,9 +1,13 @@
 <template>
     <div class ="column is-4 is-offset-4">
+        <img    alt="Vue logo" src="https://static1.millenium.us.org/articles/2/88/32/@/93282-2019-11-18-02h16-53-article_image_bd-1.png">
         <input  class="input is-danger"   v-on:input="changed" type="email"   placeholder="email account"> <br/>
         <input  class="input is-info"     v-on:input="changed" type="password"  placeholder="password"> <br/>
         <button class="button is-success" v-on:click="buttonLogin"> Log in </button>
-        <p> dede: {{ $store.getters.password }}</p>
+        <p>current price of bitcoin </p>
+        <div v-for="currency in $store.getters.password">
+            <p> {{currency.description}} {{currency.rate}}</p>
+        </div>
     </div>
 </template>
 <script>
@@ -15,7 +19,7 @@
             buttonLogin: function(event) {
                 axios
                     .get('v1/bpi/currentprice.json')
-                    .then(response => (this.$store.state.password = response))
+                    .then(response => (this.$store.state.password = response.data.bpi))
                     .catch(error => console.log(error))
             },
         }
